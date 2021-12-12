d3fae7e9969c76c8a05188ddc163910db87de594
hayashi masafumi <masafumin1515@outlook.jp>
2021-12-12 10:12:59 +0900

    fix knapsack

--- chapter_5/knapsack.cpp
+++ chapter_5/knapsack.cpp
@@ -19,11 +19,11 @@ int main() {
   for (int i=0; i<N; ++i) cin >> weight[i] >> value[i];
 
   // DP テーブル定義 dp:価値の総和の最大値
-  vector<vector<long long>> dp(N+1, vector<long long>(W+1, 0))
+  vector< vector<long long> > dp(N+1, vector<long long>(W+1, 0));
 
   // DP ループ
-  for (int i=0; i<N; ++i) {
-    for (int w=0; w<=W; ++w) {
+  for (int i = 0; i < N; ++i) {
+    for (int w = 0; w <= W; ++w) {
       // i番目の品物を選ぶ場合
       if (w-weight[i] >= 0) {
         chmax(dp[i+1][w], dp[i][w-weight[i]] + value[i]);
