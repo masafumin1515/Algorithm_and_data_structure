f1c5a4df143190f7617425b77c780b571b610ef8
hayashi masafumi <masafumin1515@outlook.jp>
2021-12-29 12:15:32 +0900

    fix scheduled interval

--- chapter_7/scheduled_interval.cpp
+++ chapter_7/scheduled_interval.cpp
@@ -4,7 +4,7 @@
 # include <functional>
 using namespace std;
 
-// 区間をpair<int, int>で表す
+// 区間をpair<int, int>で表す <始端時刻 終端時刻>
 using Interval = pair<int, int>;
 
 // 区間を終端時間で大小比較する関数
@@ -28,7 +28,7 @@ int main() {
   int res = 0;
   int current_end_time = 0;
   for (int i = 0; i < N; ++i) {
-    // 最後に選んだ区間と被るのは除く
+    // 最後に選んだ区間と被るのは除く 現時点での最短終端時間より早い開始時刻を持つ区間の場合はスキップ
     if (inter[i].first < current_end_time) continue;
 
     ++res;
