14223f7540dc751485213f27d6e7e9d184b91a35
hayashi masafumi <masafumin1515@outlook.jp>
2021-12-30 22:39:52 +0900

    linked list

--- chapter_8/linked_list.cpp
+++ chapter_8/linked_list.cpp
@@ -2,3 +2,59 @@
 # include <string>
 # include <vector>
 using namespace std;
+
+// 連結リストの各ノードを表す構造体
+struct Node {
+  Node* next;
+  string name;
+
+  Node(string name_ = "") : next(NULL), name(name_) { }
+};
+
+// 番兵を表すノードをグローバル領域に置いておく
+Node* nil;
+
+// 初期化
+void init() {
+  nil = new Node();
+  nil -> next = nil; // 初期状態ではnilがnilを指すようにする
+}
+
+// 連結リストを出力する
+void printList() {
+  Node* cur = nil -> next; // 先頭から出発
+  for (; cur != nil; cur = cur -> next) {
+    cout << cur -> name << " -> ";
+  }
+  cout << endl;
+}
+
+// ノードpの直後にノードvを挿入する
+// ノードpのデフォルト引数をnilとしておきく
+// そのため insert(v) を呼び出す操作はリストの先頭への挿入を表す
+void insert(Node* v, Node* p = nil) {
+  v -> next = p -> next;
+  p -> next = v;
+}
+
+int main() {
+  // 初期化
+  init();
+
+  // 作りたいノードの名前一覧
+  // 最後尾のノード山本から順に挿入することに注意
+  vector<string> names = {"yamamoto", "watanabe", "ito", "takahashi", "suzuki", "sato"};
+
+  // 各ノードを生成して、連結リストの先頭に挿入していく
+  for (int i = 0; i < (int)names.size(); ++i) {
+    // ノードを作成する
+    Node* node = new Node(names[i]);
+
+    // 作成したノードを連結リストの先頭に挿入する
+    insert(node);
+
+    // 各ステップの連結リストの様子を出力する
+    cout << "step" << i << ": ";
+    printList();
+  }
+}
